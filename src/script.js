bf79a2d3190d58d799eff42cd6d52fb978efa1cd
Gabriel Mureșan <32938828+gabeegabee@users.noreply.github.com>
2022-03-22 20:06:17 +0200

    Micro interactions improved

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
-
+function changeImage() {
+  if (document.getElementById("favouriteIcon").src == "src/img/heart.svg") {
+    document.getElementById("favouriteIcon").src = "src/img/heart-fill.svg";
+  } else {
+    document.getElementById("favouriteIcon").src = "src/img/heart.svg";
+  }
+}
