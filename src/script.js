2a7bf2c07432f02f25ac58e728e7310136931078
Gabriel Mureșan <32938828+gabeegabee@users.noreply.github.com>
2022-03-23 15:54:46 +0200

    Created about page, added in-dev modal

--- src/script.js
+++ src/script.js
@@ -1,7 +1,26 @@
-function changeImage() {
-  if (document.getElementById("favouriteIcon").src == "src/img/heart.svg") {
-    document.getElementById("favouriteIcon").src = "src/img/heart-fill.svg";
-  } else {
-    document.getElementById("favouriteIcon").src = "src/img/heart.svg";
-  }
+"use strict";
+
+// const heartIconContainer = document.querySelectorAll("#favouriteIconContainer");
+// const heartIcon = document.querySelectorAll("#favouriteIcon");
+
+// console.log(heartIcon, heartIconContainer);
+
+// function addToFavourites() {
+//   for (let i = 0; i <= heartIcon.length - 1; i++) {
+//     if (heartIcon[i].getAttribute("src") == "src/img/heart.svg") {
+//       heartIcon[i].src = "src/img/heart-fill.svg";
+//     } else {
+//       heartIcon[i].src = "src/img/heart.svg";
+//     }
+//   }
+// }
+
+// for (let i = 0; i <= heartIcon.length - 1; i++)
+//   heartIconContainer[i].addEventListener("click", addToFavourites);
+
+//   FIX FAVOURITE ICON
+// LOOP NOT PROPERLY WORKING, FAVOURITING ONE ASSET TURNS ON OTHER ASSET ASWELL
+
+if (window.scrollTop() > 0) {
+  document.getElementById("aboutHeading").style.position = "fixed";
 }
