f75c5a31f0202069a4be73e187c1164ff8da2490
muresan-gabriel <gabiimuresan23@gmail.com>
2022-04-22 00:27:30 +0300

    Revert to static

--- src/bad_auth.js
+++
@@ -1,6 +0,0 @@
-let loginInputContainer = document.getElementById("loginInputContainer");
-let errorMessageContainer = document.createElement("p");
-let errorMessage = "Email or password is incorrect. Please try again.";
-
-loginInputContainer.appendChild(errorMessageContainer);
-errorMessageContainer.innerHTML = errorMessage;
