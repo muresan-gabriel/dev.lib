82c899bfd57e2079d79e3acb9432500ccb51e7ad
muresan-gabriel <gabiimuresan23@gmail.com>
2022-04-21 11:51:44 +0300

    Update bad_auth.js

--- src/bad_auth.js
+++ src/bad_auth.js
@@ -1,6 +1,6 @@
 let loginInputContainer = document.getElementById("loginInputContainer");
 let errorMessageContainer = document.createElement("p");
-let errorMessage = "Email or password is incorrect.";
+let errorMessage = "Email or password is incorrect. Please try again.";
 
 loginInputContainer.appendChild(errorMessageContainer);
 errorMessageContainer.innerHTML = errorMessage;
